19cd4b3e34db387a59e8afcf8e228727c0e9b324
= <ahmadwahebarifi@gmail.com>
2026-04-02 14:10:11 +0430

    server

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-require("dotenv").config();
+require("dotenv").config({ path: '.env.local' });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("DB connected"))
   .catch(err => console.log(err));
 
