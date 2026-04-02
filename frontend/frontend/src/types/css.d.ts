389abe15ab0f3253177960346f26cf070eaa3f0a
= <ahmadwahebarifi@gmail.com>
2026-04-02 16:38:44 +0430

    css

--- frontend/frontend/src/types/css.d.ts
+++ frontend/frontend/src/types/css.d.ts
@@ -7,3 +7,13 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module '*.jsx' {
+  const Component: any;
+  export default Component;
+}
+
+declare module '*.jsx?raw' {
+  const content: string;
+  export default content;
+}
