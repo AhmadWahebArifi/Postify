e40704c824407e081e23685c3f52dc00b3f8b3ff
= <ahmadwahebarifi@gmail.com>
2026-04-02 15:07:42 +0430

    app

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Feed from './pages/Feed';
-import CreatePost from './pages/CreatePost';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import CreatePost from './components/CreatePost';
+import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/feed" element={<Feed />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/feed" element={<LandingPage />} />
         </Routes>
       </div>
     </Router>
