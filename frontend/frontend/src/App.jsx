5cce02d228575b385ae190503ee0678a4c5848cd
= <ahmadwahebarifi@gmail.com>
2026-04-02 16:37:30 +0430

    app worked

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -3,16 +3,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import CreatePost from './components/CreatePost';
 import Login from './components/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/create-post" element={<CreatePost />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/feed" element={<LandingPage />} />
+          <Route path="/" element={
+            <ProtectedRoute>
+              <LandingPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/feed" element={
+            <ProtectedRoute>
+              <LandingPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/create-post" element={
+            <ProtectedRoute>
+              <CreatePost />
+            </ProtectedRoute>
+          } />
         </Routes>
       </div>
     </Router>
