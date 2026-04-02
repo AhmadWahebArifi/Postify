6e916bb84be65f910390e4b1f720213c49ad818a
= <ahmadwahebarifi@gmail.com>
2026-04-02 14:43:16 +0430

    Server

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./routes/auth");
+const postsRoutes = require("./routes/posts");
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("DB connected"))
   .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Postify API running" });
+});
+
+app.use("/", authRoutes);
+app.use("/", postsRoutes);
 
 app.listen(5000, () => console.log("Server running"));
